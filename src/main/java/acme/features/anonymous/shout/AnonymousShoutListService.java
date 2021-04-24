a70dcdac70bce3d966360742d282ade2e02d3fb6
alvar <alvar@DESKTOP-M614NH6>
2021-04-24 20:26:25 +0200

    Completada tarea 203 Crear un Shout con un autor,un texto y un link

---
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutListService.java
@@ -0,0 +1,54 @@
+package acme.features.anonymous.shout;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.shouts.Shout;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Anonymous;
+import acme.framework.services.AbstractListService;
+
+@Service
+public class AnonymousShoutListService implements AbstractListService<Anonymous, Shout> {
+	
+	// Internal state ------------------------------------------------------
+	
+	@Autowired
+	private AnonymousShoutRepository repository;
+		
+		
+	// AbstractListService<Administrator, Shout> interface --------------------------------------------------------
+		
+		
+	@Override
+	public boolean authorise(final Request<Shout> request) {
+		assert request !=null;
+		return true;
+	}
+	
+	
+	@Override
+	public void unbind(final Request<Shout> request, final Shout entity, final Model model) {
+		assert request !=null;
+		assert entity !=null;
+		assert model !=null;
+
+		request.unbind(entity,model,"author","text","moment");
+	}
+	
+	@Override
+	public Collection<Shout> findMany (final Request<Shout> request){
+		assert request != null;
+		
+		Collection<Shout> result;
+		
+		result= this.repository.findMany();
+		
+		return result;
+	}
+		
+		
+}
