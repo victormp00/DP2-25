8aeb693eab6f04d7a851fe2a01dc03de55ec16f1
vicmonpui <victormp00@gmail.com>
2021-05-20 07:39:04 +0200

    más  tests

--- src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.DP2Test;
 
-public class ManagerTaskCreateTest extends DP2Test{
+public class ManagerTaskUpdateTest extends DP2Test{
 	@ParameterizedTest
-	@CsvFileSource(resources = "/manager/task/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/manager/task/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void createTaskNegative (final int recordIndex, final String title, final String description,
 		final String creation,final String finish,final String workload,final String link) {
 		super.signIn("manager1", "manager1");
-		super.clickOnMenu("Manager", "Create a task");
+		super.clickOnLink("Manager tasks");
+		super.clickOnListingRecord(recordIndex);
 		super.fillInputBoxIn("title", title);
 		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("creation", creation);
@@ -21,7 +22,7 @@ public class ManagerTaskCreateTest extends DP2Test{
 		super.fillInputBoxIn("workload", workload);
 		super.fillInputBoxIn("link", link);
 		super.fillInputBoxIn("publico", "true");
-		super.clickOnSubmitButton("Create");
+		super.clickOnSubmitButton("Update");
 		
 		super.checkErrorsExist();
 		super.signOut();
@@ -30,12 +31,13 @@ public class ManagerTaskCreateTest extends DP2Test{
 
 	}
 	@ParameterizedTest
-	@CsvFileSource(resources = "/manager/task/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/manager/task/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void createTaskPositive (final int recordIndex, final String title, final String description,
 		final String creation,final String finish,final String workload,final String link) {
 		super.signIn("manager1", "manager1");
-		super.clickOnMenu("Manager", "Create a task");
+		super.clickOnLink("Manager tasks");
+		super.clickOnListingRecord(recordIndex);
 		super.fillInputBoxIn("title", title);
 		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("creation", creation);
@@ -43,7 +45,7 @@ public class ManagerTaskCreateTest extends DP2Test{
 		super.fillInputBoxIn("workload", workload);
 		super.fillInputBoxIn("link", link);
 		super.fillInputBoxIn("publico", "true");
-		super.clickOnSubmitButton("Create");
+		super.clickOnSubmitButton("Update");
 		
 		super.clickOnLink("Manager tasks");
 		
