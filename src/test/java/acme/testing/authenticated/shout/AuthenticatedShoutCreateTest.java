4f2a7d73fcef1a05f9df703603a5d77a99320986
alvar <alvar@DESKTOP-8GKTLNP>
2021-05-24 18:51:27 +0200

    TestShouts Como anonimo 	-Tambien añadida restriccion del mes en la muestra de shouts 	-Añadida  correccion de creacionde  shout

---
+++ src/test/java/acme/testing/authenticated/shout/AuthenticatedShoutCreateTest.java
@@ -0,0 +1,82 @@
+package acme.testing.authenticated.shout;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.DP2Test;
+
+public class AuthenticatedShoutCreateTest extends DP2Test {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/shout/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveCreateShout(final int recordIndex,final String author, final String text, final String info) {
+		super.navigateHome();
+		super.signIn("normal1", "normal1");
+		super.clickOnMenu("Anonymous", "Shout!");
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		super.clickOnSubmitButton("Shout!");
+		
+		
+		
+	
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeCreateShout(final int recordIndex,final String author, final String text, final String info) {
+		super.navigateHome();
+		super.signIn("normal1", "normal1");
+		super.clickOnMenu("Anonymous", "Shout!");
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/shout/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveCreateShoutManager(final int recordIndex,final String author, final String text, final String info) {
+		super.navigateHome();
+		super.signIn("manager1", "manager1");
+		super.clickOnMenu("Anonymous", "Shout!");
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		super.clickOnSubmitButton("Shout!");
+		
+		
+		
+	
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeCreateShoutManager(final int recordIndex,final String author, final String text, final String info) {
+		super.navigateHome();
+		super.signIn("manager1", "manager1");
+		super.clickOnMenu("Anonymous", "Shout!");
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/acme/testing/authenticated/shout/AuthenticatedShoutListTest.java
@@ -0,0 +1,44 @@
+package acme.testing.authenticated.shout;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.DP2Test;
+
+
+public class AuthenticatedShoutListTest extends DP2Test{
+
+
+@ParameterizedTest
+@CsvFileSource(resources = "/authenticated/shout/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+@Order(10)
+public void authenticatedListPositive (final int recordIndex, final String moment, final String author, 
+	final String text) {
+	super.navigateHome();
+	super.signIn("normal1", "normal1");
+	super.clickOnMenu("Anonymous", "Shouts list");
+	
+
+	super.checkColumnHasValue(recordIndex, 0, moment);
+	super.checkColumnHasValue(recordIndex, 1, author);
+	super.checkColumnHasValue(recordIndex, 2, text);
+	super.signOut();
+	
+	}
+
+@ParameterizedTest
+@CsvFileSource(resources = "/authenticated/shout/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+@Order(20)
+public void authenticatedListPositiveManager (final int recordIndex, final String moment, final String author, 
+	final String text) {
+	super.navigateHome();
+	super.signIn("manager1", "manager1");
+	super.clickOnMenu("Anonymous", "Shouts list");
+
+	super.checkColumnHasValue(recordIndex, 0, moment);
+	super.checkColumnHasValue(recordIndex, 1, author);
+	super.checkColumnHasValue(recordIndex, 2, text);
+	super.signOut();
+	
+	}
+}
