8101cb755bf5faf217252744e1568e284115c465
vicmonpui <victormp00@gmail.com>
2021-05-31 23:45:58 +0200

    más comentarios

--- src/test/java/acme/testing/admin/DashboardShowTest.java
+++ src/test/java/acme/testing/admin/DashboardShowTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import acme.testing.DP2Test;
-
+// //Se prueba el dashboard desde el punto de vista de un andministrador ,  se considera exitoso si se muestra el dashboard con exito, el test debe dar exito
 public class DashboardShowTest extends DP2Test {
 
 	@Test
--- src/test/java/acme/testing/admin/SpamCreateTest.java
+++ src/test/java/acme/testing/admin/SpamCreateTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.DP2Test;
 
-public class SpamListCreateTest extends DP2Test {
-
+public class SpamCreateTest extends DP2Test {
+	//Se prueba la creacion de una palabra de Spam desde el punto de vista de un administrador,se prueban los paramtros : spamEn y spamEs se considera exitoso si se crea la palabra de spam con exito, el test debe dar exito
 	@ParameterizedTest
 	@CsvFileSource(resources = "/admin/spam/create-spam-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)
@@ -22,7 +22,7 @@ public class SpamListCreateTest extends DP2Test {
 		super.checkErrorsExist();
 
 	}
-
+	//Se prueba la creacion de una palabra de Spam desde el punto de vista de un administrador,se prueban los paramtros : spamEn que no puede estar en blanco y spamEs que no puede estar en blanco se considera exitoso si no se crea la palabra de spam con exito, el test debe dar exito
 	@ParameterizedTest
 	@CsvFileSource(resources = "/admin/spam/create-spam-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
