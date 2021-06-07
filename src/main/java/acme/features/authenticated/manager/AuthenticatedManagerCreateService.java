ea9042898bbecef17330c2d27bf2e3a89aa6a4e9
vicmonpui <victormp00@gmail.com>
2021-06-07 19:05:49 +0200

    español y ingles de manager y provider

--- src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
+++ src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
@@ -60,12 +60,13 @@ public class AuthenticatedManagerCreateService implements AbstractCreateService<
 		final boolean censuraCompany = Threshold.censura(entity.getCompany(), spam, threshold.getThreshold());
 		final boolean censuraSector = Threshold.censura(entity.getSector(), spam, threshold.getThreshold());
 		
-		if(censuraCompany) {
-			errors.add("company", "this company is spam ");
+		if(!errors.hasErrors("company")) {
+			errors.state(request, !censuraCompany, "company", "authenticated.manager.spam.company");
+
 		}
 
-		if(censuraSector) {
-			errors.add("sector", "this sector is spam ");
+		if(!errors.hasErrors("sector")) {
+			errors.state(request, !censuraSector, "sector", "authenticated.manager.spam.sector");
 		}
 	}
 
