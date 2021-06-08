0fc22b9c95211a2d1674fefa631fa726bac27601
Jotaeme55 <60939740+Jotaeme55@users.noreply.github.com>
2021-06-08 23:15:07 +0200

    more comits on cc tests

--- src/test/java/acme/testing/controlCheck/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/controlCheck/AnonymousShoutCreateTest.java
@@ -8,6 +8,25 @@ import acme.testing.DP2Test;
 
 public class AnonymousShoutCreateTest extends DP2Test {
 	
+	//Se prueba la creacion de un shout desde el punto de vista de un anonimo,se prueban los paramtros : author: el cual no puede ni estar en blanco, ni contener pàlabras spam, 
+		//text: no puede ni estar en blanco,ni contener pàlabras spam,info : no puede ni estar en blanco,ni contener pàlabras spam 
+		//Se considera exitoso si no se crea el shout con exito, el test debe dar exito
+		@ParameterizedTest
+		@CsvFileSource(resources = "/anonymous/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
+		@Order(20)
+		public void negativeCreateShout(final int recordIndex,final String author, final String text, final String info,final String xxxdate, final String xxxamount, final String xxxcurrency) {
+			super.navigateHome();
+			super.clickOnMenu("Anonymous", "Shout!");
+			super.fillInputBoxIn("author", author);
+			super.fillInputBoxIn("text", text);
+			super.fillInputBoxIn("info", info);
+			super.fillInputBoxIn("xxx.xxxdate", xxxdate);
+			super.fillInputBoxIn("xxx.xxxamount", xxxamount+ xxxcurrency);
+			super.clickOnSubmitButton("Shout!");
+			super.checkErrorsExist();
+			
+		}
+	
 	//Se prueba la creacion de un shout desde el punto de vista de un anonimo,se prueban los paramtros : author,text,info  se considera exitoso si se crea el shout con exito, el test debe dar exito
 	@ParameterizedTest
 	@CsvFileSource(resources = "/anonymous/shout/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
@@ -29,27 +48,7 @@ public class AnonymousShoutCreateTest extends DP2Test {
 		super.checkColumnHasValue(recordIndex, 5, xxxamount);
 		super.checkColumnHasValue(recordIndex, 6, xxxcurrency);
 		
-
-		
 	
 	}
-	//Se prueba la creacion de un shout desde el punto de vista de un anonimo,se prueban los paramtros : author: el cual no puede ni estar en blanco, ni contener pàlabras spam, 
-	//text: no puede ni estar en blanco,ni contener pàlabras spam,info : no puede ni estar en blanco,ni contener pàlabras spam 
-	//Se considera exitoso si no se crea el shout con exito, el test debe dar exito
-	@ParameterizedTest
-	@CsvFileSource(resources = "/anonymous/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(20)
-	public void negativeCreateShout(final int recordIndex,final String author, final String text, final String info,final String xxxdate, final String xxxmoney) {
-		super.navigateHome();
-		super.clickOnMenu("Anonymous", "Shout!");
-		super.fillInputBoxIn("author", author);
-		super.fillInputBoxIn("text", text);
-		super.fillInputBoxIn("info", info);
-		super.fillInputBoxIn("xxx.xxxdate", xxxdate);
-		super.fillInputBoxIn("xxx.xxxamount", xxxmoney);
-		super.clickOnSubmitButton("Shout!");
-		
-		super.checkErrorsExist();
-		
-	}
+	
 }
