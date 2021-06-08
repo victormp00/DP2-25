2b7d611a813f2a001b8f550d37a7dc9a179a7482
Enrique Salazar <enrsalmar@alum.us.es>
2021-06-08 12:12:56 +0200

    Añadidos sign up tests

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -12,9 +12,22 @@
 
 package acme.testing;
 
-public class SignUpTest {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-	// This is a placeholder where you can introduce your own sign-up test
-  	// Note that it depends on your project-specific test class.
-	
+public class SignUpTest extends DP2Test {
+
+    // Lifecycle management ---------------------------------------------------
+
+    // Test cases -------------------------------------------------------------
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @Order(10)
+    public void positiveSignUp(final String username, final String password, final String name, final String surname, final String email) {
+        super.signUp(username, password, name, surname, email);
+        super.signIn(username, password);
+        super.signOut();
+    }
 }
