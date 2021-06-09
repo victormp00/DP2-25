c86415bac3f744c76a7505e19b8a07bd4304d44d
Jotaeme55 <60939740+Jotaeme55@users.noreply.github.com>
2021-06-09 02:16:28 +0200

    done

--- src/test/java/acme/testing/controlCheck/AuthenticatedShoutCreateTest.java
+++ src/test/java/acme/testing/controlCheck/AuthenticatedShoutCreateTest.java
@@ -14,11 +14,7 @@ public class AuthenticatedShoutCreateTest extends DP2Test {
 //	, los resultados de este test son exitosos, se crean diferentes shouts que deberían poder crearse
 	
 	@ParameterizedTest
-<<<<<<< Updated upstream
-	@CsvFileSource(resources = "/authenticated/shout/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
-=======
 	@CsvFileSource(resources = "/controlCheck/authenticated/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
->>>>>>> Stashed changes
 	@Order(10)
 	public void negativeCreateShout(final int recordIndex,final String author, final String text, final String info,final String xxxdate, final String xxxamount, final String xxxcurrency) {
 		super.navigateHome();
@@ -44,39 +40,11 @@ public class AuthenticatedShoutCreateTest extends DP2Test {
 //		-El link tiene que tener el formato de url
 //		-Nada debe contener spam
 	
-	
-<<<<<<< Updated upstream
-	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(20)
-	public void negativeCreateShoutfinal( final int recordIndex,final String author, final String text, final String info,final String xxxdate, final String xxxamount, final String xxxcurrency) {
-		super.navigateHome();
-		super.signIn("normal1", "normal1");
-		super.clickOnMenu("Anonymous", "Shout!");
-		super.fillInputBoxIn("author", author);
-		super.fillInputBoxIn("text", text);
-		super.fillInputBoxIn("info", info);
-		super.fillInputBoxIn("xxx.xxxdate", xxxdate);
-		super.fillInputBoxIn("xxx.xxxamount", xxxamount+ xxxcurrency);
-		super.clickOnSubmitButton("Shout!");
-		
-		super.clickOnMenu("Anonymous", "Shouts list");
-		super.checkColumnHasValue(recordIndex, 1, author);
-		super.checkColumnHasValue(recordIndex, 2, text);
-		super.checkColumnHasValue(recordIndex, 3, xxxdate);
-		super.checkColumnHasValue(recordIndex, 5, xxxamount);
-		super.checkColumnHasValue(recordIndex, 6, xxxcurrency);
-		
-	}
-	
-	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/shout/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
-=======
 
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/controlCheck/authenticated/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
->>>>>>> Stashed changes
+
 	@Order(10)
 	public void negativeCreateShoutManager( final int recordIndex,final String author, final String text, final String info,final String xxxdate, final String xxxamount, final String xxxcurrency) {
 		super.navigateHome();
@@ -93,11 +61,7 @@ public class AuthenticatedShoutCreateTest extends DP2Test {
 	}
 
 	@ParameterizedTest
-<<<<<<< Updated upstream
-	@CsvFileSource(resources = "/authenticated/shout/negativeShout.csv", encoding = "utf-8", numLinesToSkip = 1)
-=======
 	@CsvFileSource(resources = "/controlCheck/authenticated/positiveShout.csv", encoding = "utf-8", numLinesToSkip = 1)
->>>>>>> Stashed changes
 	@Order(20)
 	public void positiveCreateShoutManager( final int recordIndex,final String author, final String text, final String info, final String xxxamount, final String xxxcurrency) {
 		final String n=new SimpleDateFormat("dd/MM/yyyy").format(new Date())+"/test"+recordIndex;
