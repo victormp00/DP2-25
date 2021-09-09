b4de7f4ab23a99e216687a4f142d867794600d31
Jose <jmtr2000@gmail.com>
2021-09-10 00:14:48 +0200

    updates

--- src/main/java/acme/features/authenticated/manager/task/ManagerTaskUpdateService.java
+++ src/main/java/acme/features/authenticated/manager/task/ManagerTaskUpdateService.java
@@ -1,3 +1,4 @@
+
 package acme.features.authenticated.manager.task;
 
 import java.util.List;
@@ -22,16 +23,15 @@ import acme.framework.services.AbstractUpdateService;
 
 @Service
 public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, Task> {
-	
 
 	@Autowired
-	protected ManagerTaskRepository repository;
+	protected ManagerTaskRepository	repository;
 	@Autowired
-	private AdminSpamRepository spamRepository;
+	private AdminSpamRepository		spamRepository;
 	@Autowired
-	private ThresholdRepository thresholdRepository;
+	private ThresholdRepository		thresholdRepository;
 
-	
+
 	@Override
 	public boolean authorise(final Request<Task> request) {
 		assert request != null;
@@ -40,12 +40,12 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		int id;
 		Manager manager;
 		boolean puedeactualizar;
-		
+
 		principal = request.getPrincipal();
-		id =  request.getModel().getInteger("id");
-		result= this.repository.findOneTaskById(id);
-		manager= result.getManager();
-		puedeactualizar=manager.getUserAccount().getId() == principal.getAccountId();
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneTaskById(id);
+		manager = result.getManager();
+		puedeactualizar = manager.getUserAccount().getId() == principal.getAccountId();
 		return puedeactualizar;
 
 	}
@@ -65,8 +65,8 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model,"title", "creation", "finish", "workload", "description", "link", "publico", "finished");
-		
+		request.unbind(entity, model, "title", "creation", "finish", "workload", "description", "link", "publico", "finished");
+
 	}
 
 	@Override
@@ -74,8 +74,8 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		assert request != null;
 		Task result;
 		int id;
-		id=request.getModel().getInteger("id");
-		result= this.repository.findOneTaskById(id);
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneTaskById(id);
 
 		return result;
 	}
@@ -91,7 +91,7 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		final Boolean censuratitle = Threshold.censura(entity.getTitle(), spam, threshold.getThreshold());
 		final Boolean censuraLink = Threshold.censura(entity.getLink(), spam, threshold.getThreshold());
 
-		if ((entity.getCreation() != null) && (entity.getFinish() != null)) {
+		
 			if (!errors.hasErrors("title")) {
 				errors.state(request, !censuratitle, "title", "manager.task.spam.title");
 			}
@@ -103,20 +103,22 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 				errors.state(request, !censuraLink, "link", "manager.task.spam.url");
 
 			}
-			if (!errors.hasErrors("creation")) {
-				errors.state(request, Boolean.TRUE.equals(entity.creationBeforeNow()), "creation", "manager.task.date2");
+			if ((entity.getCreation() != null) && (entity.getFinish() != null)) {
+				
+				if (!errors.hasErrors("creation")) {
+					errors.state(request, Boolean.FALSE.equals(entity.creationBeforeNow()), "creation", "manager.task.date2");
 
-			}
-			if (!errors.hasErrors("finish")) {
-				errors.state(request, Boolean.TRUE.equals(entity.datefit()), "finish", "manager.task.date");
+				}
+				if (!errors.hasErrors("finish")) {
+					errors.state(request, Boolean.TRUE.equals(entity.datefit()), "finish", "manager.task.date");
 
-			}
+				}
 
-			if (!errors.hasErrors("workload")) {
-				final Double doubl=100.00;
-				errors.state(request, Boolean.TRUE.equals(entity.isFit()), "workload", "manager.task.workload");
-				errors.state(request, Boolean.TRUE.equals(Task.workloadOK(entity.getWorkload())), "workload", "manager.task.workload.decimals");
-				errors.state(request, Boolean.TRUE.equals(!entity.getWorkload().equals(doubl)), "workload", "manager.task.workload.less");
+				if (!errors.hasErrors("workload")) {
+					final Double doubl=100.00;
+					errors.state(request, Boolean.TRUE.equals(entity.isFit()), "workload", "manager.task.workload");
+					errors.state(request, Boolean.TRUE.equals(Task.workloadOK(entity.getWorkload())), "workload", "manager.task.workload.decimals");
+					errors.state(request, Boolean.TRUE.equals(!entity.getWorkload().equals(doubl)), "workload", "manager.task.workload.less");
 			}
 		}
 	}
@@ -125,7 +127,6 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 	public void update(final Request<Task> request, final Task entity) {
 		assert request != null;
 		assert entity != null;
-
 
 		this.repository.save(entity);
 	}
